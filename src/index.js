8118a16279832a4fd793a84b15a13e162997bd32
Tennis Kumar C <70009826+tennis-kumar@users.noreply.github.com>
2023-02-03 19:54:20 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <div>
+    <h1 className="heading"> My Favorite Food items (few)!</h1> <br></br>
+    <div>
+      <img
+        className="food-img"
+        alt="Veggies"
+        src="https://cdn.pixabay.com/photo/2013/07/13/01/22/vegetables-155616__340.png"
+      />
+      <img
+        className="food-img"
+        alt="Raspberry"
+        src="https://cdn.pixabay.com/photo/2017/07/28/14/29/macarons-2548827__340.jpg"
+      />
+      <img
+        className="food-img"
+        alt="Noodles"
+        src="https://cdn.pixabay.com/photo/2017/11/08/22/18/spaghetti-2931846__340.jpg"
+      />
+    </div>
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
