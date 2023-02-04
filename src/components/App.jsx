5981a52e672890bb2aa98fdd57ce680ed648e400
Tennis Kumar C <70009826+tennis-kumar@users.noreply.github.com>
2023-02-04 19:31:24 +0530

    Components Practise

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-
 import Heading from "./Heading";
-import List from "./List";
 
 function App(){
+
     return (
-        <div>
-            <Heading />
-            <List />
-        </div>
+      <div>
+        <Heading />
+      </div>
     );
 }
 
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 function Heading() {
-  return <h1>My Favorite Foods</h1>;
+
+  const date = new Date();
+  const currentTime = date.getHours();
+
+  const customStyle = {
+    color: "",
+  };
+
+  let greeting;
+
+  if (currentTime >= 0 && currentTime < 12) {
+    greeting = "Good Morning";
+    customStyle.color = "Green";
+  } else if (currentTime >= 12 && currentTime < 18) {
+    greeting = "Good Afternoon";
+    customStyle.color = "Orange";
+  } else {
+    greeting = "Good Night";
+    customStyle.color = "Blue";
+  }
+
+  return (
+    <h1 className="heading" style={customStyle}>
+      {" "}
+      {greeting}
+    </h1>
+  );
 }
 
 export default Heading;
