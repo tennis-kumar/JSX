03460170387645c6fbb4a92fe7da4dee26e9de3c
Tennis Kumar C <70009826+tennis-kumar@users.noreply.github.com>
2023-02-04 18:43:38 +0530

    React Components

---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List(){
+    return (
+        <ul>
+        <li>Jamoon</li>
+        <li>Noodles</li>
+        <li>Veggies</li>
+        </ul>
+    );
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const customStyle = {
-  color: ""
-}
-
-const date = new Date();
-const curTime = date.getHours();
-
-let greeting = '';
-
-if (curTime >0 && curTime < 12) {
-  customStyle.color = "red";
-  greeting = "Good Morning!";
-} else if (curTime >= 12 && curTime < 18) {
-  customStyle.color = "green";
-  greeting = "Good Afternoon!";
-} else {
-  customStyle.color = "blue";
-  greeting = "Good Night!";
-}
+import App from "./components/App"
 
 
-
-ReactDOM.render(
-  <h1 className="heading" style={customStyle}> {greeting} </h1>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />,document.getElementById("root"));
